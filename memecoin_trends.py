8a1f68f0a45898c303454b409b3638a14880e332
VisionShaper-AI <visionshaperai@gmail.com>
2025-02-21 18:23:22 -0800

    Create memecoin_trends.py

---
+++ memecoin_trends.py
@@ -0,0 +1,15 @@
+print("""
+Hottest Trending Memecoin Name: Yzy
+
+Second Hottest Trending Memecoin Name: Pi Network
+
+Highest Market Cap Memecoin: Dogecoin ($35,816,997,127 USD)
+
+Biggest Gainer Last 24 Hours: Pi Network (887M%)
+
+Biggest Gainer Last 6 Hours: Official Eric (5M%)
+
+Biggest Gainer Last 1 Hour: Official Eric (5M%)
+
+Biggest Gainer Last 1 Hour: Swastika Coin (132K%)
+""")
